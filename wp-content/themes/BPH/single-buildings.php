91b4e5b40f8f30ede9b33acfc07baf6d1a7571a3
zachlewton <zach.g.lewton@gmail.com>
2021-05-06 16:23:26 -0400

    single-building css and added map

--- wp-content/themes/BPH/single-buildings.php
+++ wp-content/themes/BPH/single-buildings.php
@@ -9,8 +9,32 @@ $building = new TimberPost();
 
 
 $context['post'] = $post;
-// dump($context);
 
+$url = "https://nominatim.openstreetmap.org/search?format=json&limit=5&q={$post->info_address}";
+
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_POST, false);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$api_response_json = curl_exec($ch);
+curl_close($ch);
+//convert json to PHP array for further process
+$api_response_arr = json_decode($api_response_json);
+
+if($api_response_arr['respond'] == true ){
+   dump($api_response_arr);
+}else{
+    dump($api_response_arr);
+}
+
+
+
+
+
+
+// $lat = $output->results[0]->geometry->location->lat;
+// $long = $output->results[0]->geometry->location->lng;
 
 
 
