0f6e531f0271e79b362e4ff2a2fc14b3f8bffe96
danhoward <danielenriquehoward@gmail.com>
2021-05-17 13:18:21 -0400

    Remove Cruft.

--- wp-content/themes/BPH/functions/backend.php
+++ wp-content/themes/BPH/functions/backend.php
@@ -1,15 +1,9 @@
 <?php
 
 if( function_exists('acf_add_options_page') ) {
-	
 	acf_add_options_page(array(
 		'page_title' 	=> 'Global Options',
 		'menu_title'	=> 'Global Options',
 		'menu_slug' 	=> 'global-options',
-		
-		
 	));
-	
-	
-	
 }
