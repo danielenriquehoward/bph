2e6b992322cd58260348a38fff464dafb9a9b30b
danhoward <danielenriquehoward@gmail.com>
2021-05-17 13:17:48 -0400

    Clean Up CPT.

--- wp-content/themes/BPH/functions/cpt.php
+++ wp-content/themes/BPH/functions/cpt.php
@@ -1,7 +1,7 @@
 <?php
 
 function create_post_types(){
-  
+
   register_post_type('buildings',
 
   array('labels' => array(
@@ -66,44 +66,7 @@ function create_post_types(){
       'menu_icon' => 'dashicons-media-document',
       'show_in_rest' => true,
       'supports' => array('editor','title','excerpt','thumbnail'),
-      // "cptp_permalink_structure" => "/%category%/%postname%/",
       'rewrite' => array( 'slug' => '/blogs', 'with_front'=> false )
-      // 'rewrite' => array(
-      //   'slug' => '/',
-      //   'permastruct' => '/%category%/%postname%/'
-      // ),
-    )
-  );
-
-
-  register_post_type(
-    'bio',
-    array(
-      'labels' => array(
-        'name'                       => _x( 'Bio', 'taxonomy general name', 'textdomain' ),
-        'singular_name'              => _x( 'Bio', 'taxonomy singular name', 'textdomain' ),
-        'search_items'               => __( 'Search Bio', 'textdomain' ),
-        'popular_items'              => __( 'Popular Bio', 'textdomain' ),
-        'all_items'                  => __( 'All Bio', 'textdomain' ),
-        'parent_item'                => null,
-        'parent_item_colon'          => null,
-        'edit_item'                  => __( 'Edit Bio', 'textdomain' ),
-        'update_item'                => __( 'Update Bio', 'textdomain' ),
-        'add_new_item'               => __( 'Add New Bio', 'textdomain' ),
-        'new_item_name'              => __( 'New Bio Name', 'textdomain' ),
-        'separate_items_with_commas' => __( 'Separate Bio with commas', 'textdomain' ),
-        'add_or_remove_items'        => __( 'Add or remove Bio', 'textdomain' ),
-        'choose_from_most_used'      => __( 'Choose from the most used Bio', 'textdomain' ),
-        'not_found'                  => __( 'No Bio found.', 'textdomain' ),
-        'menu_name'                  => __( 'Bio', 'textdomain' ),
-      ),
-      'public' => true,
-      'has_archive' => false,
-      'hierarchical' => false,
-      'taxonomies' => array('post_tag', 'what_to_do', 'category'),
-      // 'menu_icon' => 'dashicons-location-alt',
-      'supports' => array('none',),
-      // 'rewrite' => array( 'slug' => 'guides', 'with_front'=> true )
     )
   );
 
@@ -133,148 +96,10 @@ function create_post_types(){
       'has_archive' => false,
       'hierarchical' => false,
       'taxonomies' => array('post_tag', 'what_to_do', 'category'),
-      // 'menu_icon' => 'dashicons-location-alt',
       'supports' => array('title',),
-      // 'rewrite' => array( 'slug' => 'guides', 'with_front'=> true )
     )
   );
 
-
-
-  register_post_type(
-    'quotes',
-    array(
-      'labels' => array(
-        'name'                       => _x( 'Quotes', 'taxonomy general name', 'textdomain' ),
-        'singular_name'              => _x( 'Quote', 'taxonomy singular name', 'textdomain' ),
-        'search_items'               => __( 'Search Quotes', 'textdomain' ),
-        'popular_items'              => __( 'Popular Quotes', 'textdomain' ),
-        'all_items'                  => __( 'All Quotes', 'textdomain' ),
-        'parent_item'                => null,
-        'parent_item_colon'          => null,
-        'edit_item'                  => __( 'Edit Quote', 'textdomain' ),
-        'update_item'                => __( 'Update Quote', 'textdomain' ),
-        'add_new_item'               => __( 'Add New Quote', 'textdomain' ),
-        'new_item_name'              => __( 'New Quote Name', 'textdomain' ),
-        'separate_items_with_commas' => __( 'Separate Quotes with commas', 'textdomain' ),
-        'add_or_remove_items'        => __( 'Add or remove Quotes', 'textdomain' ),
-        'choose_from_most_used'      => __( 'Choose from the most used Quotes', 'textdomain' ),
-        'not_found'                  => __( 'No Quotes found.', 'textdomain' ),
-        'menu_name'                  => __( 'Quotes', 'textdomain' ),
-      ),
-      'public' => true,
-      'has_archive' => false,
-      'exclude_from_search' => true,
-      'hierarchical' => false,
-      'menu_icon' => 'dashicons-format-quote',
-      'supports' => array('title')
-    )
-  );
 
 }
 add_action('init', 'create_post_types');
-
-
-////////////////////////////////////////////////////////////////////
-/////////////////////// CUSTOM TAXONOMIES //////////////////////////
-////////////////////////////////////////////////////////////////////
-
-
-// function registerwhattodo() {
-
-//   // Add Class taxonomy, make it hierarchical (like categories)
-//   $labels = array(
-//     'name'              => _x( 'What To Do', 'taxonomy general name' ),
-//     'singular_name'     => _x( 'What To Do', 'taxonomy singular name' ),
-//     'search_items'      => __( 'Search What To Do' ),
-//     'all_items'         => __( 'All What To Do' ),
-//     'parent_item'       => __( 'Parent What To Do' ),
-//     'parent_item_colon' => __( 'Parent What To Do:' ),
-//     'edit_item'         => __( 'Edit What To Do' ),
-//     'update_item'       => __( 'Update What To Do' ),
-//     'add_new_item'      => __( 'Add New What To Do' ),
-//     'new_item_name'     => __( 'New What To Do Name' ),
-//     'menu_name'         => __( 'What To Dos' ),
-//   );
-//   $args = array(
-//     'labels'            => $labels,
-//     'public'            => true,
-//     'query_var'         => false,
-//     'hierarchical'      => false,
-//     'show_ui'           => false,
-//     'show_admin_column' => false,
-//     //'rewrite'           => array( 'slug' => '/what-to-do', 'with_front' => false)
-//   );
-//   register_taxonomy( 'what_to_do', array(
-//     'articles',
-//   ), $args );
-
-
-// }
-// add_action( 'init', 'registerwhattodo' );
-
-
-/* //// Listing Types Custom Taxonomy //// */
-
-// function registerlistingtype() {
-
-//   // Add Class taxonomy, make it hierarchical (like categories)
-//   $labels = array(
-//     'name'              => _x( 'Listing Type', 'taxonomy general name' ),
-//     'singular_name'     => _x( 'Listing Type', 'taxonomy singular name' ),
-//     'search_items'      => __( 'Search Listing Type' ),
-//     'all_items'         => __( 'All Listing Type' ),
-//     'parent_item'       => __( 'Parent Listing Type' ),
-//     'parent_item_colon' => __( 'Parent Listing Type:' ),
-//     'edit_item'         => __( 'Edit Listing Type' ),
-//     'update_item'       => __( 'Update Listing Type' ),
-//     'add_new_item'      => __( 'Add New Listing Type' ),
-//     'new_item_name'     => __( 'New Listing Type Name' ),
-//     'menu_name'         => __( 'Listing Types' ),
-//   );
-//   $args = array(
-//     'labels'            => $labels,
-//     'public'            => true,
-//     'query_var'         => true,
-//     'hierarchical'      => true,
-//     'show_ui'           => true,
-//     'show_admin_column' => true,
-//     'rewrite'           => array( 'slug' => '/listing-type', 'with_front' => false)
-//   );
-//   register_taxonomy( 'listing_type', array(
-//     'listings',
-//   ), $args );
-
-
-// }
-// add_action( 'init', 'registerlistingtype' );
-
-
-
-// nested nav dsacve for later @jamesgillispie
-
-// function menu_nested_pages() {
-//   $post = new TimberPost();
-//   wp_list_pages(
-//     array(
-//       'post_type' => $post->post_type,
-//       'child_of'  => $post->ID,
-//       'orderby'   => 'menu_order',
-//       'order'     => 'ASC',
-//       'depth'     => 0,
-//       'title_li'  => '' // hides parent
-//     )
-//   );
-// }
-// function is_menu_nested_pages() {
-//   $post = new TimberPost();
-//   $current_post_id = $post->ID;
-//   wp_list_pages(
-//     array(
-//       'post_type' => $post->post_type,
-//       'child_of'  => $post->post_ancestors,
-//       'title_li' => '',
-//       // 'exclude' => $current_post_id
-//     )
-//   );
-// }
